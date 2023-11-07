587b74aa16e7bb4c0790b33f38d22e9a0e742e7b
Tony <102321536+TonyNguyy@users.noreply.github.com>
2023-11-07 09:42:52 -0500

    Updated Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState, useEffect, useRef, FormEvent} from "react"
-import OpenAI from "openai";
+import { useState, useEffect, useRef, FormEvent } from 'react'
+import OpenAI from 'openai';
+import Sphere from "./assets/sphere.png";
+import Fireflies from "./assets/fireflies.gif"
 
-
-
-
-const App = () => {
-  const [placeholder,setPlaceHolder]= useState("");
-  const [prompt, setPrompt]= useState('')
-  const [result,setResult] = useState('')
-  const [loading,setLoading] = useState(false)
-
+function App() {
+  const [placeholder,setPlaceHolder] = useState("");
+  const [prompt,setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+ 
   const phrases = [
     "A pineapple in the ocean",
     "Astronaut on the moon with a beer",
@@ -18,8 +17,8 @@ const App = () => {
   ]
 
   const typingSpeed = 100
-  const deletingSpeed = 50
-  const delayBeforeTyping = 1000
+  const deletingSpeed = 50 
+  const delayBeforeTyping = 1000 
 
   const typingTimeRef = useRef<NodeJS.Timeout | null>(null);
 
@@ -82,7 +81,6 @@ const App = () => {
       })
       setLoading(false)
       setResult(res.data[0].url!)
-      console.log(result)
     } catch (error) {
       setLoading(false)
       if (typeof error === "string"){
@@ -94,34 +92,54 @@ const App = () => {
       }
     }
   }
-  return (
-    <div className="w-screem h-screen bg-black flex justify-center
-    items-center flex-col p-10 text-center">
-      <h6 className="text-2xl text-white semi-bold mb-4">Welcome to </h6>
-     <h1 className="text-5xl font-bold
+
+
+  return ( 
+  
+    <div className="w-screen h-screen bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900 relative overflow-hidden">
+       <div>
+         <img src={Fireflies} alt="fireflies" className='z-0 absolute w-full h-full' />
+         <img src={Sphere} alt="sphere" className='w-[600px] absolute lg:top-0 lg:right-4 animate-pulse hidden lg:block drop-shadow-2xl' />
+         <img src={Sphere} alt="sphere" className='w-[350px] absolute lg:bottom-10 animate-pulse hidden lg:block drop-shadow-2xl' />
+         <img src={Sphere} alt="sphere" className='w-[400px] absolute lg:bottom-[100px] lg:right-[5px] animate-pulse hidden lg:block drop-shadow-2xl' />
+         <img src={Sphere} alt="sphere" className='w-[500px] absolute lg:top-[150px] lg:left-30 animate-pulse hidden lg:block drop-shadow-2xl' />
+         <img src={Sphere} alt="sphere" className='w-[350px] absolute lg:top-20 lg:left-1 animate-pulse hidden lg:block drop-shadow-2xl ' />
+       </div>
+       <div className='flex justify-center align-middle
+        items-center flex-col p-10 text-center z-10 relative mt-12'>
+           <h6 className="text-2xl text-white semi-bold mb-4">Welcome to </h6>
+       <h1 className="text-5xl font-bold
      bg-gradient-to-r from-pink-500 via-yellow-300 to-green-300
      text-transparent bg-clip-text uppercase mb-3 animate-pulse">Render-AI</h1>
     <span className="text-white semi-bold">Create Images using Artificial Intelligence</span>
-    
+     
 
-    <form className="mt-10 flex">
-      <input value={prompt} onChange={handleChange} type="text" placeholder={placeholder} className="w-80 rounded-md px-1" />
+   <form className="mt-10 flex">
+      <input value={prompt} onChange={handleChange} type="text" placeholder={placeholder} className="w-80 rounded-md px-2" />
       <button className="" onClick={generateImage}>
         <span className="bg-gradient-to-r from-pink-500 via-yellow-300 to-green-300
      text-transparent bg-clip-text uppercase mx-4 py-1 px-3 text-xl cursor-pointer bold">Create ✨</span>
       </button>
     </form>
+       </div>    
+     
     {loading ? (
-        <div className=" text-5xl font-bold uppercase bg-gradient-to-r from-pink-500 via-yellow-300 to-green-300 text-transparent bg-clip-text mt-10">
+        <div className=" text-center items-center text-5xl font-bold uppercase bg-gradient-to-r from-pink-500 via-yellow-300 to-green-300 text-transparent bg-clip-text mt-10">
           Loading...
         </div>
       ) : (
         <></>
       )}
 
-      {result.length > 0 ? <img className="mt-10 mb-2" src={result} alt="Generated Image" /> : <></>}
-
-
+      {result.length > 0 ? 
+      
+ 
+        <div className='text-center items-center flex justify-center mb-4'>
+        <img className="mt-10 mb-8 rounded-md relative" src={result} alt="Generated Image" />
+        </div>
+   
+      : <></>}
+   
     </div>
   )
 }
